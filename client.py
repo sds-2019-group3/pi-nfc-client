e83d2267b933c8e482b1eae1420fa9312d1731f1
Andrew Taylor <andrew_t97@hotmail.co.uk>
2019-03-23 16:58:43 +0000

    Modified to accept room name instead of boolean

--- client.py
+++ client.py
@@ -22,14 +22,13 @@ print("Connected on " + ip + ":" + str(port))
 
 # Loop
 while True:
-    # Receive 4 bytes of data over the socket
-    # The only thing ever sent through is "true" when we can unlock the door
-    # "true" = 4 chars, hence 4 bytes
-    data = s.recv(4)
-    # If we get some data, we can know the door we're allowed to enter the room
-    if data:
+    # Get the length of the room name so we know how many bytes to expect
+    string_length = int(s.recv(4))
+    # Get the room name
+    room = s.recv(string_length)
+    if room:
         # Download files or whatever in here, go mental
-        print("Unlock received, lovely stuff")
+        print("Unlock received for room '" + room + "', lovely stuff")
     else:
         # In this case, we have received some null data. Only bad things can cause this such as unplugging the USB cable, or quitting the server phone-side
         print("Something went wrong, maybe the connection broke? Shutting down.")
