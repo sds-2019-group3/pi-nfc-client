5899cfd4a0350656a4ea3d7ffe88e0e36e1b0822
Andrew Taylor <andrew_t97@hotmail.co.uk>
2019-03-23 17:14:00 +0000

    Added exception catches

--- client.py
+++ client.py
@@ -22,14 +22,18 @@ print("Connected on " + ip + ":" + str(port))
 
 # Loop
 while True:
-    # Get the length of the room name so we know how many bytes to expect
-    string_length = int(s.recv(4))
-    # Get the room name
-    room = s.recv(string_length)
-    if room:
-        # Download files or whatever in here, go mental
-        print("Unlock received for room '" + room + "', lovely stuff")
-    else:
+    try:
+        # Get the length of the room name so we know how many bytes to expect
+        string_length = int(s.recv(4))
+        # Get the room name
+        room = s.recv(string_length)
+        if room:
+            # Download files or whatever in here, go mental
+            print("Unlock received for room '" + room + "', lovely stuff")
+    except ValueError: 
         # In this case, we have received some null data. Only bad things can cause this such as unplugging the USB cable, or quitting the server phone-side
         print("Something went wrong, maybe the connection broke? Shutting down.")
+        break
+    except KeyboardInterrupt:
+        print("Received keyboard interrupt. Shutting down.")
         break
